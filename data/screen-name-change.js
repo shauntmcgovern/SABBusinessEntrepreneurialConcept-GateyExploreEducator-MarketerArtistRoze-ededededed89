602821a4f905f4314f12f2059f4268944989478a
Interim President of the USA <shauntmcgovern@gmail.com>
2022-05-13 15:05:23 -0500

    Add files via upload

---
+++ data/screen-name-change.js
@@ -0,0 +1,72 @@
+window.YTD.screen_name_change.part0 = [
+  {
+    "screenNameChange" : {
+      "accountId" : "1350156112021090304",
+      "screenNameChange" : {
+        "changedAt" : "2021-01-15T19:14:33.000Z",
+        "changedFrom" : "RozeSantos13",
+        "changedTo" : "RozeSantos4510"
+      }
+    }
+  },
+  {
+    "screenNameChange" : {
+      "accountId" : "1350156112021090304",
+      "screenNameChange" : {
+        "changedAt" : "2021-07-01T17:21:33.000Z",
+        "changedFrom" : "RozeSantos4510",
+        "changedTo" : "ShaunTMcGovern"
+      }
+    }
+  },
+  {
+    "screenNameChange" : {
+      "accountId" : "1350156112021090304",
+      "screenNameChange" : {
+        "changedAt" : "2022-02-23T19:51:01.000Z",
+        "changedFrom" : "ShaunTMcGovern",
+        "changedTo" : "SchawnMaGug"
+      }
+    }
+  },
+  {
+    "screenNameChange" : {
+      "accountId" : "1350156112021090304",
+      "screenNameChange" : {
+        "changedAt" : "2022-02-25T19:16:20.000Z",
+        "changedFrom" : "SchawnMaGug",
+        "changedTo" : "ShaunTMcGovern"
+      }
+    }
+  },
+  {
+    "screenNameChange" : {
+      "accountId" : "1350156112021090304",
+      "screenNameChange" : {
+        "changedAt" : "2022-02-26T18:06:55.000Z",
+        "changedFrom" : "ShaunTMcGovern",
+        "changedTo" : "interimPresiden"
+      }
+    }
+  },
+  {
+    "screenNameChange" : {
+      "accountId" : "1350156112021090304",
+      "screenNameChange" : {
+        "changedAt" : "2022-02-26T18:13:02.000Z",
+        "changedFrom" : "interimPresiden",
+        "changedTo" : "IntPresident"
+      }
+    }
+  },
+  {
+    "screenNameChange" : {
+      "accountId" : "1350156112021090304",
+      "screenNameChange" : {
+        "changedAt" : "2022-03-24T18:50:44.000Z",
+        "changedFrom" : "IntPresident",
+        "changedTo" : "INT_President"
+      }
+    }
+  }
+]
