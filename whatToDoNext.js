31f3193f61a076f4d367a457ba5e04a4a139eabc
Interim President of the USA <shauntmcgovern@gmail.com>
2022-06-14 17:03:56 -0500

    june 4th abstract tutorial

---
+++ whatToDoNext.js
@@ -0,0 +1,60 @@
+This one can be about chemistry:
+Especially the history of Chemistry:
+https://en.wikipedia.org/wiki/History_of_chemistry
+Then get into one the first modern chemist, and therefore one of the founders of modern chemistry, 
+and one of the pioneers of modern experimental scientific method.
+https://en.wikipedia.org/wiki/Robert_Boyle
+As it did come into discovery in the 1600s
+Which in even earlier times chemistry was associated with mysticism && alchemy which for 
+even any subject that deals with these very subjects, it would entail more demonstrative powers of the laws of nature && ours.
+
+This could be sumthin' new.
+But as I remember
+how
+it
+was
+before.
+These men were already
+outcasted
+by the
+good
+innocent
+&& very pleasing
+no matter
+the caliber
+women
+by
+us.
+The poor men of good
+who faced
+ridiculous
+amounts
+of sexual deprivation
+but that
+was over
+half a decade ago
+already
+&& all they had was money
+so it was very easy to
+rape a person
+What I mean to say is
+All the women
+are safe
+For good.
+Thanks to be
+Allah.
+For that.
+As well.
+We have a good reminder
+of all those nasty
+recurring dreams.
+Where they hook onto us
+with no
+solution.
+Look no further.
+There.
+I had it once.
+Now I am famous.
+
+It could also be about fault lines. Which has to do with geology.
+https://en.wikipedia.org/wiki/Fault_(geology)
