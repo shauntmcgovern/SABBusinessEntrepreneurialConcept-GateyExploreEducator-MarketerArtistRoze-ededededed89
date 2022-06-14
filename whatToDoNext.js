3d882ba6206f9b902ceab397b66f903966abc99e
Interim President of the USA <shauntmcgovern@gmail.com>
2022-06-14 17:18:18 -0500

    june 5th abstractional

--- whatToDoNext.js
+++ whatToDoNext.js
@@ -58,3 +58,52 @@ Now I am famous.
 
 It could also be about fault lines. Which has to do with geology.
 https://en.wikipedia.org/wiki/Fault_(geology)
+<!--                                         -->
+A godess, Aphrodite: 
+https://en.wikipedia.org/wiki/Aphrodite
+why does history always have a pecuilar way of decribing these
+Goddesses. An ancient Greek Goddess. Greek mythology. Hinduism. Buddhism. Aztec mythology.
+Maya mythology. Roze begins to see the similarity but at the same time, peoples, defined it
+in other words than other peoples on the different side of the world. With different advantages.
+https://en.wikipedia.org/wiki/Aztec_mythology
+https://en.wikipedia.org/wiki/Maya_mythology
+https://en.wikipedia.org/wiki/Ancient_Greek_religion
+
+When You Just
+Dont Care
+&& You Shake It All About
+&& You do a song called, "Hokey Pokey"
+&& You realize what they have done to your body
+&& That its full game.
+You have nothing to guard it.
+You cannot afford a guard system.
+This is poor town && you live in it.
+So be it. As it has been said.
+Is it because you have no choice in the matter?
+Is that why your question is filled like Air in your body?
+Does it matter what I do with the answer?
+Thas' good. Well that was alot of fun.
+As they say.
+Cheerio is another but I would like to see you try?
+Would any good man make it all this way so far?
+You could say that.
+Thanks. I said
+Goodbye.
+To-la lou?
+Have a good time. I'll be back here when you see me.
+Meet me at home.
+Alright.
+What does that mean.
+It means what you think it means.
+Then its my next favourite thing O My God.
+When it came to this, what did you do?
+Well I had it once. Then tried another time.
+When could I come over you say?
+When was the last time?
+Why couldnt it of happened this way?
+C-Ya Later.
+
+Reppin' @CHANEL && @maybelline because this is what the female needs && thinks about.
+Then begin to talk about the skincare && always looking good for the outside world mostly.
+Girls spend most of their time making themselves up for others. Then mostly their man. 
+I guess you could say. Their just like everybody else.
