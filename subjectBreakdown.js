2700b86e1c2c9eb4c4d0ab6c30dfdc0152d0b41a
Interim President of the USA <shauntmcgovern@gmail.com>
2022-07-20 11:25:26 -0500

    bibliography.

--- subjectBreakdown.js
+++ subjectBreakdown.js
@@ -1,40 +1,97 @@
-April 11, 2022.     Chex Cereal, Twitter.           C, T.
-April 13, 2022.     Valve Software.                 V.
-April 15, 2022.     NASA.                           N.
-April 16, 2022.     Meta, w3c, Mountain Dew, @LILUZIVERT, @PostMalone, @yeat1_          M, W, MD, U, P, Y.
-April 19, 2022.     Topology.                       T.
-April 20, 2022.     Activision, Steam, Lu Lu Lemon, Sesame Street, Cubs.                A, S, L, SS, C.
-April 21, 2022.     Hawaiian Punch, Chanel, Bethesda, Whole Foods, Dollar Tree.         H, C, B, W, D.
-April 22, 2022.     Circumference.                  C.
-April 23, 2022.     Perception.                     P.
-April 25, 2022.     Hololens, Chance the Rapper, Kanye West, Augmented Reality.         H, C, K, A.
-April 26, 2022.     Nike, Apple Jacks, Chase, Circadian Rhythm.                         N, A, C, CR.
-April 27, 2022.     Photobiology.                   P.
-April 29, 2022.     Aerodynamics.                   A.
-                    Unity, ROM Image.               U, RI.
-April 30, 2022.     Sonemic, Glitchwave Games, CinemosFilm, YoutubeMusic, Spotify.      S, GG, CF, YM, S.       [Fashion Showcase].
-May 2, 2022.        @MyVibrantHealth, Google, Photobiology, Nutrition.                  VH, G, P, N.
-May 3, 2022.        @NEIU, @ChiCityColleges, Sonar, @MyVibrantHealth, WhatsApp, Nike, Apple.        N, CC, S, VH, W, N, A.
-                    H.P. Lovecraft, The Dunwich Horror, Necronomicon.                   HL, DH, N.              [Fashion Showcase].
-May 4, 2022.        Surgical Instruments.           S.
-May 5, 2022.        Nutritional Science, Amtrak.    A, NS.
-May 6, 2022.        Pyramid Head.                   P.
-May 7, 2022.        Randomness.                     R.
-May 8, 2022.        @Wikipedia, Magic.              M, W.
-May 9, 2022.        Chanel, Arizona Tea, Johnson && Johnson.                            A, C, JJ.
-May 10, 2022.       Chicago Police, CIA, Homan Square Facility.                         CP, CI, HS.
-May 11, 2022.       @JODYHIGHROLLER, Agricultural Machinery, Topograhy                  AM, J, T.
-May 13, 2022.       Spotify.                        S.              [Fashion Showcase].
-May 14, 2022.       @Twitter, Thomas Edison.        T, TE.
-May 15, 2022.       Wolfram Alpha, Yamaha, Personal Watercrafts.                        PW, WA, Y.
-May 16, 2022.       Wolfram Alpha, Stephen King, Faygo.                                 F, SK, WA.              [Fashion Showcase].
-May 17, 2022.       Biochemistry.                   BC.
-May 18, 2022.       Planetary Science.              PS.
-                    Landscaping, Landscape Engineering, Faygo, Chicago Bulls, John Deere.           CB, F, JD, L, LE.       [Fashion Showcase].
-May 19, 2022.       Aztec Empire.                   A.
-May 20, 2022.       Sleep, Chase, Hoverboard.       C, H, S.
-May 21, 2022.       Generative Grammar, Jet Propulsion, Scanners, Nintendo.             G, J, N, S.
-May 23, 2022.       Cubs, John Deere, McLaren, Tyler the Creator, Oceanography.         C, J, M, O, T.          [Fashion Showcase].
+April 11, 2022.     Chex Cereal, Twitter.           
+C, T.
+April 13, 2022.     Valve Software.                 
+V.
+April 15, 2022.     NASA.                           
+N.
+April 16, 2022.     Meta, w3c, Mountain Dew, @LILUZIVERT, @PostMalone, @yeat1_          
+M, W, MD, U, P, Y.
+April 19, 2022.     Topology.                       
+T.
+April 20, 2022.     Activision, Steam, Lu Lu Lemon, Sesame Street, Cubs.                
+A, S, L, SS, C.
+April 21, 2022.     Hawaiian Punch, Chanel, Bethesda, Whole Foods, Dollar Tree.         
+H, C, B, W, D.
+April 22, 2022.     Circumference.                  
+C.
+April 23, 2022.     Perception.                     
+P.
+April 25, 2022.     Hololens, Chance the Rapper, Kanye West, Augmented Reality.
+H, C, K, A.        
+April 26, 2022.     Nike, Apple Jacks, Chase, Circadian Rhythm.                         
+N, A, C, CR.
+April 27, 2022.     Photobiology.                   
+P.
+April 29, 2022.     Aerodynamics.                   
+A.
+                    Unity, ROM Image.               
+U, RI.
+April 30, 2022.     Sonemic, Glitchwave Games, CinemosFilm, YoutubeMusic, Spotify.      [Fashion Showcase].
+S, GG, CF, YM, S.
+May 2, 2022.        @MyVibrantHealth, Google, Photobiology, Nutrition.                  
+VH, G, P, N.
+May 3, 2022.        @NEIU, @ChiCityColleges, Sonar, @MyVibrantHealth, WhatsApp, Nike, Apple.        
+N, CC, S, VH, W, N, A.
+                    H.P. Lovecraft, The Dunwich Horror, Necronomicon.                   [Fashion Showcase].
+HL, DH, N. 
+May 4, 2022.        Surgical Instruments.           
+S.
+May 5, 2022.        Nutritional Science, Amtrak.    
+A, NS.
+May 6, 2022.        Pyramid Head.                   
+P.
+May 7, 2022.        Randomness.                     
+R.
+May 8, 2022.        @Wikipedia, Magic.             
+M, W.
+May 9, 2022.        Chanel, Arizona Tea, Johnson && Johnson.                            
+A, C, JJ.
+May 10, 2022.       Chicago Police, CIA, Homan Square Facility.                         
+CP, CI, HS.
+May 11, 2022.       @JODYHIGHROLLER, Agricultural Machinery, Topograhy                  
+AM, J, T.
+May 13, 2022.       Spotify.                        [Fashion Showcase].
+S.
+May 14, 2022.       @Twitter, Thomas Edison.        
+T, TE.
+May 15, 2022.       Wolfram Alpha, Yamaha, Personal Watercrafts.                        
+PW, WA, Y.
+May 16, 2022.       Wolfram Alpha, Stephen King, Faygo.                  				[Fashion Showcase].
+F, SK, WA.
+May 17, 2022.       Biochemistry.                   
+BC.
+May 18, 2022.       Planetary Science.              
+PS.
+                    Landscaping, Landscape Engineering, Faygo, Chicago Bulls, John Deere.           			[Fashion Showcase].
+CB, F, JD, L, LE.       
+May 19, 2022.       Aztec Empire.                   
+A.
+May 20, 2022.       Sleep, Chase, Hoverboard.       
+C, H, S.
+May 21, 2022.       Generative Grammar, Jet Propulsion, Scanners, Nintendo.             
+G, J, N, S.
+May 23, 2022.       Cubs, John Deere, McLaren, Tyler the Creator, Oceanography.         [Fashion Showcase].
+C, J, M, O, T. 
+May 29, 2022. 		Chicago Bulls, Jordans, MerriamWebster, HBO, Deadwood(TV Series), Transformers, Basketball.	[Fashion Showcase].
+B, CB, D, H, J, M, T.
+May 31, 2022. 		Psychology, Early Modern Human.
+EH, P.
+June 1, 2022.		McDonalds, Jewel Osco, Aldi, Whole Foods, Mathematical Notation, Bering Strait.
+A, BS, JO, MN, M, W.
+June 2, 2022. 		Accuweather, NEIU, Arizon Tea, Young God Records, Weather.
+A, AT, N, W, YG.
+June 3, 2022. 		@lululemon, @Nike, Disney Studios, Magnet							[Fashion Showcase].
+DS, L, M, N.
+June 22, 2022.		Robert Boyle, History of Chemistry, Fault(geology).
+F, HC, R.
+June 23, 2022.		Aztec Mythology, Ancient Greek Religion, Persephone, Aphrodite, Maya Mythology.
+A, AG, AM, MM, P.
+					Lucky Charms, Bugatti, @DaveandBusters, Headquarters, Chance the Rapper, FTW Chicago, @HighlineChicago, Coors Light, Drake, Arcade Game.
+AG, B, CL, CR, D, DB, FC, H, HL, LC.
+June 24, 2022.		7UP, Sprite, King James, Chief David Brown, Chicago Police, FBI Chicago, CIA, Bird.
+7, B, C, CD, CP, FC, KJ, S.
+
+
  
 
 
