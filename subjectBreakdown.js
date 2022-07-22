19864ca8974e2a34a025aa7e71df40748233b1c6
Interim President of the USA <shauntmcgovern@gmail.com>
2022-07-22 11:51:20 -0500

    Update subjectBreakdown.js

--- subjectBreakdown.js
+++ subjectBreakdown.js
@@ -1,16 +1,16 @@
 April 11, 2022.     Chex Cereal, Twitter.           
 C, T.
-April 13, 2022.     Valve Software.                 
-V.
+April 13, 2022.     Valve Software. Roc Nation. Gucci Mane. MIT.
+GM, M, RN, V.
 April 15, 2022.     NASA.                           
 N.
 April 16, 2022.     Meta, w3c, Mountain Dew, @LILUZIVERT, @PostMalone, @yeat1_          
 M, W, MD, U, P, Y.
 April 19, 2022.     Topology.                       
 T.
-April 20, 2022.     Activision, Steam, Lu Lu Lemon, Sesame Street, Cubs.                
+April 20, 2022.     Activision, Steam, Lu Lu Lemon, Sesame Street, Cubs.		[Fashion Showcase].                
 A, S, L, SS, C.
-April 21, 2022.     Hawaiian Punch, Chanel, Bethesda, Whole Foods, Dollar Tree.         
+April 21, 2022.     Hawaiian Punch, Chanel, Bethesda, Whole Foods, Dollar Tree.         [Fashion Showcase].
 H, C, B, W, D.
 April 22, 2022.     Circumference.                  
 C.
@@ -72,81 +72,81 @@ May 21, 2022.       Generative Grammar, Jet Propulsion, Scanners, Nintendo.
 G, J, N, S.
 May 23, 2022.       Cubs, John Deere, McLaren, Tyler the Creator, Oceanography.         [Fashion Showcase].
 C, J, M, O, T. 
-May 29, 2022. 		Chicago Bulls, Jordans, MerriamWebster, HBO, Deadwood(TV Series), Transformers, Basketball.	[Fashion Showcase].
+May 29, 2022. 	    Chicago Bulls, Jordans, MerriamWebster, HBO, Deadwood(TV Series), Transformers, Basketball.	[Fashion Showcase].
 B, CB, D, H, J, M, T.
-May 31, 2022. 		Psychology, Early Modern Human.
+May 31, 2022. 	    Psychology, Early Modern Human.
 EH, P.
-June 1, 2022.		McDonalds, Jewel Osco, Aldi, Whole Foods, Mathematical Notation, Bering Strait.
+June 1, 2022.	    McDonalds, Jewel Osco, Aldi, Whole Foods, Mathematical Notation, Bering Strait.
 A, BS, JO, MN, M, W.
-June 2, 2022. 		Accuweather, NEIU, Arizon Tea, Young God Records, Weather.
+June 2, 2022. 	    Accuweather, NEIU, Arizon Tea, Young God Records, Weather.
 A, AT, N, W, YG.
-June 3, 2022. 		@lululemon, @Nike, Disney Studios, Magnet			[Fashion Showcase].
+June 3, 2022.       @lululemon, @Nike, Disney Studios, Magnet				[Fashion Showcase].
 DS, L, M, N.
-June 22, 2022.		Robert Boyle, History of Chemistry, Fault(geology).
+June 22, 2022.	    Robert Boyle, History of Chemistry, Fault(geology).
 F, HC, R.
-June 23, 2022.		Aztec Mythology, Ancient Greek Religion, Persephone, Aphrodite, Maya Mythology.
+June 23, 2022.	    Aztec Mythology, Ancient Greek Religion, Persephone, Aphrodite, Maya Mythology.
 A, AG, AM, MM, P.
-			Lucky Charms, Bugatti, @DaveandBusters, Headquarters, Chance the Rapper, FTW Chicago, @HighlineChicago, Coors Light, Drake, Arcade Game.
+		    Lucky Charms, Bugatti, @DaveandBusters, Headquarters, Chance the Rapper, FTW Chicago, @HighlineChicago, Coors Light, Drake, Arcade Game.
 AG, B, CL, CR, D, DB, FC, H, HL, LC.
-June 24, 2022.		7UP, Sprite, King James, Chief David Brown, Chicago Police, FBI Chicago, CIA, Bird.
+June 24, 2022.	    7UP, Sprite, King James, Chief David Brown, Chicago Police, FBI Chicago, CIA, Bird.
 7, B, C, CD, CP, FC, KJ, S.
-June 25, 2022. 		Ubuntu, Microsoft Windows, Operating System.
+June 25, 2022. 	    Ubuntu, Microsoft Windows, Operating System.
 MW, OS, U.
-			Bioshock, Hideo Kojima, Death Stranding, @RealGDT, Overwatch, Overwatch League, Blizzard, Rockstar Games, Grand Theft Auto III, The Rock, DOOM,
-			John Carmack, MSDOS, Quake, Unreal Tournament, Battlefield.
+		    Bioshock, Hideo Kojima, Death Stranding, @RealGDT, Overwatch, Overwatch League, Blizzard, Rockstar Games, Grand Theft Auto III, The Rock, DOOM,
+		    John Carmack, MSDOS, Quake, Unreal Tournament, Battlefield.
 Ba, Bi, Bl, D, DS, GA, GT, HK, JC, M, O, OL, RG, R, Q, UT.
-June 28, 2022. 		Nintendo, Sega, Mario Kart, Plastic, Pixar, Face.
+June 28, 2022. 	    Nintendo, Sega, Mario Kart, Plastic, Pixar, Face.
 F, MK, N, Pi, Pl, S.
-			Computer Monitor, Being And Nothingness, Alien Anthology, Alien(film).
+		    Computer Monitor, Being And Nothingness, Alien Anthology, Alien(film).
 AA, A, BN, CM.
-			Microsoft, Nintendo, @OnDeck, Sega, Steam, Skeleton, History of Video Game Consoles, The Ruins of Beverast, Youtube Music.
+		    Microsoft, Nintendo, @OnDeck, Sega, Steam, Skeleton, History of Video Game Consoles, The Ruins of Beverast, Youtube Music.
 HC, M, N, O, RB, Se, Sk, St, YM.
-June 29, 2022. 		Medicine, Sports Medicine, Endocrinology, Immunology, Pathology, Neuroscience, Police Brutality in the United States, Killjoy(film series).
+June 29, 2022. 	    Medicine, Sports Medicine, Endocrinology, Immunology, Pathology, Neuroscience, Police Brutality in the United States, Killjoy(film series).
 E, K, I, M, N, P, PS, SM.
-June 30, 2022. 		DC Comics, Roots of the Swamp Thing, Decomposition.
+June 30, 2022. 	    DC Comics, Roots of the Swamp Thing, Decomposition.
 D, DC, RT.
-July 1, 2022.		Chicago, Metra, Greyhound Buses, Amtrak, CTA, Midway Airport, O'Hare Airport, Navy Pier, Cubs, White Sox, Chicago Bulls, @NHLBlackhawks,
-			Chicago Fire, Chicago Bears, UIC, @UChicago, Northwestern, NEIU, Metropolis.
+July 1, 2022.	    Chicago, Metra, Greyhound Buses, Amtrak, CTA, Midway Airport, O'Hare Airport, Navy Pier, Cubs, White Sox, Chicago Bulls, @NHLBlackhawks,
+		    Chicago Fire, Chicago Bears, UIC, @UChicago, Northwestern, NEIU, Metropolis.
 C, M, GB, A, C, MA, OA, NP, C, WS, CB, B, CF, CB, U, UC, No, N, M.
-July 3, 2022.		@PsychToday, Mind-wandering, Intrusive Thought.
+July 3, 2022.	    @PsychToday, Mind-wandering, Intrusive Thought.
 IT, MW, PT.
-			Brain, Cycling.
+		    Brain, Cycling.
 B, C.
-July 5, 2022. 		@usedgov, @SIUC, @HW_College, @NEIU, @ElginCommColl, @ChiCityColleges, Education in the United States, H. R. Giger.
+July 5, 2022. 	    @usedgov, @SIUC, @HW_College, @NEIU, @ElginCommColl, @ChiCityColleges, Education in the United States, H. R. Giger.
 CC, EC, ES, HC, HG, N, S, U.
-July 6, 2022. 		@USCIS, Drug Cartel, Che Guevara, Immigration to the United States, ATF Gunwalking Scandal, Barack Obama, @POTUS45.
+July 6, 2022. 	    @USCIS, Drug Cartel, Che Guevara, Immigration to the United States, ATF Gunwalking Scandal, Barack Obama, @POTUS45.
 AS, BO, CG, DC, DT, IS, US.
-July 7, 2022. 		Paradigm, The Structure of Scientific Revolutions, Mircosoft, Issac Asimov, Three Laws of Robotics.
+July 7, 2022. 	    Paradigm, The Structure of Scientific Revolutions, Mircosoft, Issac Asimov, Three Laws of Robotics.
 IA, M, P, SR, TR.
-July 8, 2022. 		NEIU, ChiCityColleges, Semelparity && iteroparity, Natural Selection, Charles Darwin.
+July 8, 2022. 	    NEIU, ChiCityColleges, Semelparity && iteroparity, Natural Selection, Charles Darwin.
 CC, CD, N, NS, S.
-July 9, 2022.		Wheatgrass, JambaJuice, Poaceae, ChiCityColleges, NEIU, Great Plains.
+July 9, 2022.	    Wheatgrass, JambaJuice, Poaceae, ChiCityColleges, NEIU, Great Plains.
 CC, GP, JJ, N, P, W.
-July 10, 2022. 		Chicago Police, @FBIChicago, CIA, @thisisUIC, NEIU, Atrium, Wikipedia, @chipublib, Cavaedium, Obama's Foundation, 
-			Barack Obama, Michelle Obama, Dior, Logitech, Apple.
+July 10, 2022. 	    Chicago Police, @FBIChicago, CIA, @thisisUIC, NEIU, Atrium, Wikipedia, @chipublib, Cavaedium, Obama's Foundation, 
+		    Barack Obama, Michelle Obama, Dior, Logitech, Apple.
 A, At, BO, Ca, Ch, Ci, CP, D, F, L, MO, N, OF, U, W.
-July 11, 2022.		NEIU, ChiCityColleges, Stethoscope, Tool, Stereoscope, Carl Jung, Platypus.
+July 11, 2022.	    NEIU, ChiCityColleges, Stethoscope, Tool, Stereoscope, Carl Jung, Platypus.
 CC, CJ, N, P, Stet, Ster, T.
-July 12, 2022. 		NEIU, ChiCityColleges, Chicago Reader, IMDb, Miramax, Lionsgate, Film, Mickey Rourke, Gasper Noe.
+July 12, 2022. 	    NEIU, ChiCityColleges, Chicago Reader, IMDb, Miramax, Lionsgate, Film, Mickey Rourke, Gasper Noe.
 CC, CR, F, GN, I, L, M, MR, N.
-July 13, 2022.		Six Flags, NEIU, ChiCityColleges, Barack Obama, Revenant, Spirit(vital essence), Emanuel Swedenborg, Ghost, Apparitional Experience.
+July 13, 2022.	    Six Flags, NEIU, ChiCityColleges, Barack Obama, Revenant, Spirit(vital essence), Emanuel Swedenborg, Ghost, Apparitional Experience.
 AE, BO, CC, ES, G, N, R, S, SF.
-July 14, 2022. 		SIUC, ChiCityColleges, NEIU, Barack Obama, Chicago, @SUBWAY, Arachnophobia, Acrophobia, Dromophobia, Taphophobia, Phasmophobia, Necrophobia, 
-			Anglophobia, Aichmophobia, Claustrophobia, @NSAGov, Ophidiophobia, Vegetation.
+July 14, 2022. 	    SIUC, ChiCityColleges, NEIU, Barack Obama, Chicago, @SUBWAY, Arachnophobia, Acrophobia, Dromophobia, Taphophobia, Phasmophobia, Necrophobia, 
+		    Anglophobia, Aichmophobia, Claustrophobia, @NSAGov, Ophidiophobia, Vegetation.
 Ac, Ai, An, Ar, BO, CC, Ch, Cl, D, N, Ne, Ns, O, P, S, Su, T, V.
-July 15, 2022.		NEIU, Drake, Jainism, Stanza, ChiCityColleges, Philosophy.
+July 15, 2022.	    NEIU, Drake, Jainism, Stanza, ChiCityColleges, Philosophy.
 CC, D, J, N, P, S.
-July 16, 2022.		NEIU, ChiCityColleges, Albatross, Albertsons, Aberystruth, Clown.
+July 16, 2022.	    NEIU, ChiCityColleges, Albatross, Albertsons, Aberystruth, Clown.
 Ab, Alba, Albe, CC, Cl, N.
-July 17, 2022.		@CCSOPIO, Chicago Police, NEIU, ChiCityColleges, Jody Highroller, Barack Obama, Donald Trump, @POTUS Joe Biden, Justification(jurisprudence),
-			Criminal Charge, Conviction.
+July 17, 2022.	    @CCSOPIO, Chicago Police, NEIU, ChiCityColleges, Jody Highroller, Barack Obama, Donald Trump, @POTUS Joe Biden, Justification(jurisprudence),
+		    Criminal Charge, Conviction.
 BO, C, CCh, CCs, CC, CP, DT, JB, JH, J, N.
-July 18, 2022. 		Oxford Philosophy, Yale Psychology, EPA, Kleenex, @PsychToday, ChiCityColleges, NEIU, Ocean, January, Karl Jaspers.
+July 18, 2022. 	    Oxford Philosophy, Yale Psychology, EPA, Kleenex, @PsychToday, ChiCityColleges, NEIU, Ocean, January, Karl Jaspers.
 CC, E, J, K, KJ, N, O, OP, PT, YP.
-July 19, 2022.		NEIU, Parachute, Submarine, @USATODAY, @hotelsdotcom, @Airbnb, Periscope, @NBA, Whistle, ChiCityColleges, Barack Obama, Nike.
+July 19, 2022.	    NEIU, Parachute, Submarine, @USATODAY, @hotelsdotcom, @Airbnb, Periscope, @NBA, Whistle, ChiCityColleges, Barack Obama, Nike.
 A, Bo, CC, Ho, Nb, Ne, Ni, Pa, Pe, S, U, W.
-July 20, 2022. 		CIA, @FBIChicago, Chicago Police, diarray, Bread of Life Discourse, Civil Disorder, @WonderBreadUSA, @HW_College, NEIU, Jody Highroller, 
-			Barack Obama, @kettlebrand, Sandwich.
+July 20, 2022. 	    CIA, @FBIChicago, Chicago Police, diarray, Bread of Life Discourse, Civil Disorder, @WonderBreadUSA, @HW_College, NEIU, Jody Highroller, 
+		    Barack Obama, @kettlebrand, Sandwich.
 BD, BO, CD, DP, C, D, HC, FC, JH, KB, N, S, WU.
 
 
