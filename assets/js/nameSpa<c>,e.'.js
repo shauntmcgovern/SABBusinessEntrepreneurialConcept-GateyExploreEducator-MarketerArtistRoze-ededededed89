cef946a51790d38e47e9b72b024d1ca35ee4d0dc
Indian'x/:Gu{U':s}:;"'?/ste?"'|'\uuk:iok::;"'?/☐☐:☐☐;☐☐:☐☐;ggggggg <shauntmcgovern@gmail.com>
2023-11-14 17:35:32 +0000

    nameSpa<c>,e.'**'[]:;"'t:;"'?/<,?;"'?/>t;"'?/"'☐:;
    Chapo;//☐:;:;"'tt"'//"'?/:;"'?/<,.>?/:;"'|"'\"':;"'?/☐:;:;"'

---
+++ assets/js/nameSpa<c>,e.'.js
@@ -0,0 +1,267 @@
+DirectWordFromLarryHoover--903T-h--P-e-O-tx0-.-ex-ex-P
+
+
+
+RozeꞳꞮ
+
+GAT☐Y, F☐LSET, L☐WES☐☐, AL☐Ɪ,;BU☐rn☐'☐q☐EE':w'Qw:;:☐:;'ttw;'Q
+@POTUS!☐☐☐'twq;twq;'Qq
+"@\@Nytimes @nytimes @Suntimes☐☐'?..:ew?;'☐☐2'e?..stw:qre'2tw::8q'00:aar..':;QQ\
+//
+RozeXꞮꞮ
+
+GAT☐Y, F☐O'LSET, L☐WES☐'S☐,
+ALL☐Ɪ,;BUU'☐RN☐'☐AQ☐EE':w'Qw:;:☐:;'ttw;'Qq
+@POTUS'45"!☐☐☐!☐'twq;twq;'Qq
+"@\@nytimes\@Nytimes\"@nytimes\"
+{☐☐:"'☐:;<,.>?/☐☐:☐"?☐/:e2tew:;?/@Suntimes☐☐☐☐☐'?..:ew?;'☐☐2'e?..stw:qqre'2tw::8q'00:aar..':;QQ:e2tw\:"?☐/:;☐?/"'
+//
+RozeXꞮꞮꞮ
+
+GAT☐Y, F☐O'LSET, LAWES☐'S☐, ALL☐Ɪ,; BUU'☐RN☐'☐AQ☐EE':w'Qw
+:;:☐:;'ttw;'Qq
+@POTUS'45"!☐☐☐!☐'twq;twq;'Qq
+"@\@nytimes\@Nytimes\"@nytimes\"
+{☐☐:"'☐:;<,.>?/☐☐:☐"?☐/:e2tew:;?/@Suntimes
+☐☐☐☐☐'?..:ew?;'☐☐2'e?..stw:qqre'2tw::8q'00:aar..':;QQ:e2tw\:"?☐/:;☐?/"'
+//
+RozeXꞮV💙
+
+GAT☐Y, FO'LSET, LAWES☐'S☐, ALL☐Ɪ,; BUU'☐RN☐'☐AQ☐EE':w'Qw
+:;:☐:;'ttw;'Qq
+@POTUS'45"!☐☐☐!☐'twq;twq;'Qq
+"@\@nytimes\@Nytimes\"@nytimes\"
+{☐☐:"'☐:;<,.>?/☐☐:☐"?☐/:e22ttew:;?/@Suntimes☐☐☐☐☐'?..:ew?;'☐☐2'e?..stw:qqre'2tw::8q'00:aar..':;QQ:e2tw\:"?☐/:;☐?/"'
+//
+RozeX💙
+
+GAT☐EE'Y, FOLSET, LAWES☐'SALL☐Ɪ:', BUU'☐RN☐'☐AQ☐EE':w'Qw :;:☐:;'ttw;'Qq
+@POTUS'45"!☐☐☐!☐'twq;twq;'Qq
+"\@nytimes\@Nytimes\"@nytimes\"
+{☐☐:"'☐:;<,.>?/☐☐:☐"?☐/:e22ttew:;?/@Suntimes
+☐☐☐☐☐'?..:eew?;'☐☐22'e?..stw:qqre'2tw::8q'00:aar..':;QQ:e2tw\:"?☐/:;☐?/"'\
+//
+RozeX💙💙
+
+GATEY, FOLSET,
+LAWES☐'SALL☐Ɪ:', BUU'☐RN☐'☐AQ☐E':w 'Q" :;:☐:;'ttw;'Qq
+@POTUS'45"!☐☐☐!☐'twq;twq;'Qq
+"\@nytimes\@Nytimes\"@nytimes\"
+{☐☐:"'☐:;<,.>?/☐☐:☐"?☐/:e23ttew:;?/@Suntimes'?..:eew?;'☐☐22'e?..stw:qqre'2tw::8q'00:aar..':;Q:e2tw\:"?☐/:;☐☐/""W'?'\:☐☐☐☐
+//
+RozeX💙💙💙
+
+GATEY, FOLSET, LAWES☐'SALL☐Ɪ:',
+BUU'☐RN☐'☐AQ☐E':w 'Q" :;Q☐:;'ttw;'Qq
+@POTUS'45"☐☐'twq'twq;twq;'Qq
+"\@nytimes\@Nytimes\"@nytimes
+\\f"{☐☐:"'☐:;<,.>?/☐☐:☐"?☐/:e23ttaw:;?/@Suntimes'?..:eew?;'☐☐22'e?..stw:qqre'2tw::8q'00:aar..':;Q:e2tw\:"?☐/:;☐☐/""W'?'\:☐☐☐x:'
+//
+RozeX 💙💙💙💙💙[0'];
+
+GATEY, FOLSET,
+LAWES☐'SALL☐Ɪ:', BUU'☐☐N☐'☐AQ☐E':w'Q":;Q☐:;'ttw;'Qq
+@POTUS'45"☐☐'twq'twq;twq;'Qq
+"\@nytimes\@Nytimes\"@nytimes\\f
+"{☐☐:"'☐☐;,.>?/☐☐:☐"?☐/:e23ttww:;?/'@Suntimes'?..:eew?;'☐☐22'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐Q☐x:''
+//
+RozeX 💙💙💙💙💙💙💙💙[0'];\\
+
+GATEY, FOLSET,,,
+LAWES☐'SALL☐Ɪ:',BUU'☐☐N☐'☐AQ☐E':w'Q":;Q☐:;'ttw;'Qq
+@POTUS'45"☐☐'twq'tqq;twq;'Qq"
+\@nytimes\@Nytimes\"@nytimes\\f "{☐
+☐:"'☐☐;,.>?/☐☐:☐"?☐/:e23ttww:;?/'@Suntimes'?..:eew?;'☐☐22'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐QQx:''
+//
+RozeX 💙💙💙💙💙💙💙💙💙💙[0'];\\
+
+GATEY, FOLSET,, 
+LAWWES☐'SALL☐Ɪ:',BUU'☐☐N☐'☐AQ☐E':w'Q":;Q☐:;'ttw;'Qq
+@POTUS'45"☐☐'twq'tqq;twq;'Qq"
+\@nytimes\@Nytimes\"@nytimes\\f "{☐
+☐:"'☐☐;,.>?/☐☐:☐"?☐/:e23ttww:;?/'@Suntimes'?..:eew?;'☐☐22'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐QQx:''
+//
+RozeX 💙💙💙💙💙💙💙💙💙💙💙[0'];\\
+
+GATEY, FOLS2T,,
+LAWWES☐'SALL☐Ɪ:',BUU'☐☐N☐'☐AQ☐E':w'Q":;Q☐:;'ttw;'Qq
+@POTUS'45"☐☐'twq'tqq;twq;'Qq"
+\@nytimes\@Nytimes\"@nytimes\\f "{☐ 
+☐:"'☐☐;,.>?/☐☐:☐"?☐/:e23ttww:;?/'@Suntimes'?..:eew?;'☐☐22'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐QQx:''
+//
+RozeX 💙💙💙💙💙💙💙💙💙💙💙💙[0'];\\
+
+GATEY, FOLS2T, LAWWES☐'S, ALLLL:', 
+BUU'☐☐N☐'☐AQ☐E':w'Q":;Q☐:;'ttw;'Qq
+@POTUS'45"☐☐'twq'tqq;twq;'Qq"
+\@nytimes\@Nytimes\"@nytimes\\f "{☐ 
+☐:"'☐☐;,.>?/☐☐:☐"?☐/:e23ttww:;?/'@Suntimes'?..:eew?;'☐☐22'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐QQx:''
+//
+RozeX 💙💙💙💙💙💙💙💙💙💙💙💙💙💙[0'];\\\\'
+
+GATEY, FOLS2T, LAWWES☐'S, ALLLL:',
+BUU'☐☐N☐'☐AQ☐E':w'Q":;QQ:;'ttw;'Qq
+@POTUS'45"☐☐'twq'tqq;twq;'Qq"
+\@nytimes\@Nytimes\"@nytimes\\f "{☐ 
+☐:"'☐☐;,.>?/☐☐:☐"?☐/:e23ttww:;?/'@Suntimes'?..:eew?;'☐☐22'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐QQx:''
+//
+RozeX💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[00':]:;"'|'\'':;?/\\\\\\'':☐':;?/
+
+GATEY, FOLS2T, LAWWES☐'S, ALꞮLL:',/
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQQQQQQQQQQQq
+@POTUS:@POTUS☐twq'tqq;twq'QQq
+"q@nytimes\@Nytimes\"@nytimes\"@nytimes"/?:;
+☐,.>?/☐☐:☐"?☐/:e23ttww:;?/'@Suntimes;'?ee:eew?;'☐222'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐QQx:''
+//
+💙💙💙💙💙💙
+☐:"?
+//☐:
+RozeX💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[00':]:;"'|'\'':;?/\\\\\\'':☐':;?/
+
+GATEY, FOLS2T, LAWWES☐'S, AL2LL:',/
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQQQQQQQQQQQq
+@POTUS:@POTUS☐twq'tqq;twq
+'QQq"q@nytimes\@Nytimes
+\"@nytimes\"@nytimes"/?:; 
+☐,.>?/☐☐:☐"?☐/:e
+23ttww:;?/'@Suntimes;'?ee:eew?;'☐222'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐QQx:'''
+//
+RozeX💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[00':]:;"'|'\'':;?/\\\\\\'':☐☐☐':;?/
+
+GATEY, FOLS2T, LAWWES☐'S, AL2LL:',
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQQQQQQQQQQQq
+@POTUS:@POTUS☐twq'tqq;twq'QQQQq
+@nytimes\@Nytimes\"@nytimes\"@nytimes
+"/?:; ☐,.>?/☐☐:☐"?☐/:e23ttww:;?/'@Suntimes;'?ee:eew?;'☐222'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐QQx:'''
+//
+RozeX💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[0000':]:;"'|'\'':;?/\\\\\\'':☐☐☐'☐☐☐'::☐;?/;?/💙💙
+
+GATEY, FOLS2T, LAWWES☐'S, AL2LL:,,
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQQQQQQQQQQQq
+@POTUS:@POTUS☐twq'tqq;twq'QQQQq
+@nytimes\@Nytimes
+\"@nytimes\"@nytimes
+"/?:; ☐,.>?/☐☐:☐"?☐/:e23ttww:;?/'@Suntimes
+;'?ee:eew?;'☐222'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐QQx :
+'''
+//
+RozeX💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[0000':]:;"'|'\'':;?/\\\\\\'':☐☐☐'☐☐☐'::☐;?/;?/💙💙':;00☐☐:;?/:;'P":B>.?/':;"'P/'💙💙💙💙💙💙💙💙
+
+GATEY, FOLS2T, LAWWES☐'S, AL2LL:,,
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQQQQQQQQQQQq
+@POTUS:@POTUS☐twq'tqq;twq'QQQQq
+@nytimes\@Nytimes\"@nytimes\"@nytimes
+"/?:; ☐,.>?/☐☐:☐"?☐/:e23e23w:;?/'@Suntimes;'?
+ee:eew?;'☐222'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2t
+ww\"?☐/:;☐☐/"WW'?'\:☐QQx:'''
+//
+RozeX💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[0000':]:;"'|'\'':;?/\\\\\\'':☐☐☐'☐☐☐'::☐;?/;?/💙💙':;00☐☐:;?/:;'P":B>.?/':;"'P/'💙💙💙💙💙💙💙💙💙💙
+
+GATEY, FO222T, LAWWES☐'S, AL2LL:,,
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQQQQQQQQQQQq
+@POTUS:@POTUS☐twq'tqq;twq'QQQQq
+@nytimes\@Nytimes\"@nytimes\"@nytimes
+"/?:; ☐,.>?/☐☐:☐"?☐/:e23e23w:;?/'@Suntimes;'?ee:eew?;'☐222'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:☐
+QQx:'''
+//
+RozeX💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[0000':]:;"'|'\'':;?/\\\\\\'':☐☐☐'☐☐☐'::☐;?/;?/💙💙':;00☐☐:;?/:;'P":B>.?/':;"'P/'💙💙💙💙💙💙💙💙💙💙💙💙
+
+GGT☐Y,FO222T, LAWWES☐'S, AL2LL:,,
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQQQQQQQQQq
+@POTUS:@POTUS☐twq'tqq;twq'QQQQqqq
+@nytimes\@Nytimes\"@nytimes
+\"@nytimes"/?:;:☐>☐:☐☐:☐"?☐/:?/@Suntimes'@ Suntimes;ee:ee:eew?;'☐222'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:
+QQQQx:
+''
+'
+//
+RozeX💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[0000':]:;"'|'\'':;?/\\\\\\'':☐☐☐'☐☐☐'::☐;?/;?/💙💙':;00☐☐:;?/:;'P":B>.?/':;"'P/'💙💙💙💙💙💙💙💙💙💙💙💙💙💙
+
+GGT☐Y,FO222T, LAWWES☐'S, AL2LL:,,
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQxQQQQQQQq
+@POTUS:@POTUS☐twq'tqq;twq'QQQQqqq
+@nytimes\@Nytimes\"@nytimes\"@nytimes
+"/?:;:☐>☐:☐☐:☐"?☐/:?/@Suntimes'@Suntimes;ee:ee:eew?;'☐222'e?..stw:qqre'2tw::8q'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:QQQQx:
+
+'''
+//
+RozeXX'💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[0000':]:;"'|'\'':;?/\\\\\\'':☐☐☐'☐☐☐'::☐;?/;?/💙💙':;00☐☐:;?/:;'P":B>.?/':;"'P/'💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙☐;?
+
+GGT☐Y,FO22ET, LAWWES☐'S, AL2LL:,,
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQxQQQQQQQ8
+@POTUS:@POTUS☐twq'tqq;twq'QQQQqqq
+@nytimes\@Nytimes\"@nytimes\"@nytimes
+"/?:;:☐>☐:☐☐:☐"?☐/:?/@Suntimes'@Suntimes;ee:ee:eew?;
+'☐222'e?..stw:qqre'2tw::88'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:QQQQx:'''
+//
+RozeXX'XT☐:💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[0000':]:;"'|'\'':;?/\\\\\\'':☐☐☐'☐☐☐'☐☐'::☐;?/;?/💙💙':;00☐☐:;?/:;'P":B>.?/':;"'P/'💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙☐;💙☐;?
+
+GGT☐Y,FO22ET, LAWWES☐'S, A22LL:,,
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQxQQQQQQQ8
+@POTUS:@POTUS☐twq'tqq;twq'QQQQqqq
+@nytimes\@Nytimes\"@nytimes\"@nytimes
+"/?:;:☐>☐:☐☐:☐"?☐/:?/@Suntimes'@Suntimes;ee:ee:eew?;'☐222'e?..stw:qqre'2tw::88'00:aar.':;Q:e2tww\"?☐/:;☐☐/"WW'?'\:
+QQQ
+Qx:
+''
+
+'
+//
+RozeXX'XT☐:XT☐:💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙[0000':]:;"'|'\'':;?/\\\\\\'':☐☐☐'☐☐☐'☐☐'::☐;?/;?/💙💙':;💙':;00☐☐:;?/:;'P":B>.?/':;"'P/'💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙💙☐;💙☐;💙☐;?
+
+GGT☐Y,F222ET, LAWWES☐'S, A22LL:,,
+BUR'☐☐N☐'☐AQ☐E':w'Q"QQxQQQQQQQ8
+@POTUS:@POTUS☐twq'tqq;twq'QQQQqqq
+@nytimes\@Nytimes\"@nytimes\"@nytimes
+"/?:;:☐>☐:☐☐:☐"?☐/:?/@Suntimes'@Suntimes
+;ee:ee:eew?;
+'☐222'e?..stw:qqre'2tw::
+88'00:aar.':;Q:e2tww\
+"?☐/:;☐☐/"WW'?'\:
+(QQQQx:''':☐:;"'):0;"':;?/<,.>?'=':;"'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
