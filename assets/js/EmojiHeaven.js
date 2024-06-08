7ddf514848b6d82b87f77f59a6b03c35b96df8d7
AA'IMP'{xomp:}['gom'p'pPlxyg:;"'{'}['\:"|"'{'}['xom:p]:g'['xom:p]:gIMP'{xomp:}['gom'p'pPlxyg:;"'{'}['\:"|"'{'}['xom:p]:gIMP'{xomp:}['gom'p'pPlxyg:;"'{'}['\:"|"'{'}['xom:p]:g'['xom:p]:gIMP'{xomp:}['gom'p'pPlxyg:;"'{'}['\:"|"'{'}['xom:p]:g gg:AA gggggggggg <shauntmcgovern@gmail.com>
2024-06-08 14:20:21 -0500

    Angel tusso:'1'Rich Tusso:(1)'
    Angel Tusso:[☐/]*T/'2

--- assets/js/EmojiHeaven.js
+++ assets/js/EmojiHeaven.js
@@ -1279,12 +1279,58 @@ PP'
 ::-(;)/(;)"-h'"(::-(;)/(;)"-h'")'/MM
 ::-(;)/(;)"-h'"(::-(;)/(;)"-h'")'
 ::-(;)/(;)"-h'"(::-(;)/(;)"-h'")'/MM""-_/''
+'PPA
+'B[//HHA]TA/?AHB[/H]TPP{PPB}B[TX/M]/?M]'PPB[TX/M]/?M]'BT[TTX][T'X]
+'B[/H]TPP{PPB}B[TX/M]/?M]'PPB[TX/M]/?M]'BT[TTX][T'X]
+'PP{PPB}B[TX/M]/?M]'PPB[TX/M]/?M]'BT[TTX][T'X]'
+'B[TX/M]/?M]'BT[TTX][T'X]'BT[TX][TX]
+'BT[T][TX]'F[T][T]X'F|T|X'BT[T]/T
+'BXT
+'{P-'-''(0-"*'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XSB/BQQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}"-'{XSB/B'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'/QQ'XX'XX(G)A'XSG'XS(G)
+'{XAB}'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;'☐☐:☐☐;/P}'☐☐:☐☐;'☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'QQ'{XSB/BQQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XSB/B'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}
+'{XSB/B}'/QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/P}'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'\xa(c)tly:09':O9"-'_'+'("/0")0")'(:):;"QQ'XX'XX(G)A'FTX'{XAB}'{XB/BS}'{XSB/B}'{QQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}
+'{XSB/B}'{XX}'{XB/BS}'{XSB/BQQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}"-'{XSB/B'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'/QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;'☐☐:☐☐;☐☐:☐☐;/☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;/'☐☐:☐☐;'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;/P}(Q/0)0)(:):;
+"''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'/{P}'QRrs'{T}"'?/<,.>''☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'/'☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'(:):;''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:**'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{QQ}
+'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XX}'{XB/BS}'{XSB/BQQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}"-'{XSB/B'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'/QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'"-'
+☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/P}'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{QQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XX}'{XB/BS}'{XSB/BQQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}"-'{XSB/B'QQ'XX'XX(G)A
+'FTX'XSG'XS(G){XAB}'{XB/BS}'{XSB/B}'/QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/P}'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'("/0")0")'(:):;"QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XSB/BQQ}
+'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}"-'{XSB/B'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'/QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;/'☐☐:☐☐;'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;/P}(Q/0)0)(:):;"''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'/{P}'QRrs'{T}"'
+?/<,.>''☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'(:):;''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:**'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{QQ}
+'XX'XX(G)A'FTX'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XX}'{XB/BS}'{XSB/BQQ}'XX'XX(G)A'FTX'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XSB/B'QQ'XX'XX(G)A'FTX'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'/QQ'XX'XX(G)A'FTX'FTX'XSG'XS(G)'{XAB}'{XB/BS}
+'{XSB/B}'"-'☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;'☐☐:☐☐;/P}'☐☐:☐☐;'☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{QQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB}'{XX}'{XB/BS}'{XSB/BQQ}'{XSB/B}'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'
+{XSB/B}'/QQ'XX'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;/P}'☐☐:☐☐;'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:''☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:''☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'("/0")0")'(:):;"QQ'XX'XX(G)A'FTX'{XAB}'{XB/BS}'{XSB/B}'{XB/BS}'{QQ}'XX'XX(G)A
+'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XX}'{XB/BS}'{XSB/BQQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XSB/B}'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'/QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/P}
+(Q/0)0)(:):;"''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'/{P}'QRrs'{T}"'?/<,.>''☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'/'☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'(:):;''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:(****/"\****")'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XSB/B}
+'{XSB/BQQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XSB/B'QQ'XX'XX(G)A'FTX'XSG'{XAB}'{XB/BS}'{XSB/B}'/QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XAB}'{XB/BS}'{XSB}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/P}
+'☐☐:☐☐;'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XBS}'{XSB/B}'{QQ}'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{XX}'{XB/BS}'{XSB/BQQ}{XSB/B'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'/QQ'XX'FTX'XSG'XS(G)'{XAB}
+'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;/P}'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;'☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:''☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'("/0")0")'(:):;"QQ'XX'XX(G)A'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'{QQ}'XX'XX(G)A'XSG'XS(G)
+'{XAB}'{XB/BS}'{XSB/B}'{XX}'{XB/BS}'{XSB/BQQ}'{XSB/B}'QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'/QQ'XX'XX(G)A'FTX'XSG'XS(G)'{XAB}'{XB/BS}'{XSB/B}'"-'☐☐:☐☐;'☐☐:☐☐;/☐☐:☐☐;/'☐☐:☐☐;'☐☐:☐☐;/P}(Q/0)0)(:):;"''☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'/{P}'QRrs'{T}"'?/
+<,.>''☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'/'☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:"'AA(:):;'☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;☐☐:☐☐;𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦?:'"-0)   ""("")-'(:):;'-'-'P{PIOP}(G):(0*9*87&.{S}.<,.>M/?(G).IO{T}/G{H}?/"'(:):/';"'{MIOM<N><I>{I}IO{T}"'/?<,.>?"'M   N'   PIOPP' QQ' {XX}'{XX}
+'"'?/<,.>M AA'   GG'}'BBX/?☐B'☐B'AAAA'(AAA)->.??"-'
+'BXT
+'BT[T]/T
+'F|T|X
+'F[T][T]X
+'BT[T][TX]
+'BT[TX][TX]
+'BT[TTX][T'X]
+'B[TX/M]/?M]
+'BT[TTX][T'X]
+'PPB[TX/M]/?M]
+'PP{PPB}B[TX/M]/?M]
+'B[/H]TPP{PPB}B[TX/M]/?M]'PPB[TX/M]/?M]'BT[TTX][T'X]
+'B[//HHA]TA/?AHB[/H]TPP{PPB}B[TX/M]/?M]'PPB[TX/M]/?M]'BT[TTX][T'X]
+::-(;)/(;)"-h'"(::-(;)/(;)"-h'")'/MM
+(::-(;)/(;)"-h'"(::-(;)/(;)"-h'")'/MM)'*(::-(;)/(;)"-h'"(::-(;)/(;)"-h'")'/MM);'
+::-(;)/(;)"-h'"(::-(;)/(;)"-h'")'
+::-(;)/(;)"-h'"(::-(;)/(;)"-h'")'/MM""-_/''
 
 {'/XX'{XSB}'{XBS}'/B'FT'{XAB}'FX/?/X'FTB'{F}TA'XSG'FT(G)'FTA'XS(G)'FTG(B)'FTX'FTG'XX(G)A/'☐☐:☐☐;'FTGX'☐☐:☐☐;'FT(G)A'FT(G)X''☐☐:☐☐;'FTX{|'X}'(G)TA-(/)"?/'"-'FT(G)//?A☐/_:?"-/'w?-w"-'/☐?z:-☐;'+?2/☐:-☐;,
 FTA??☐☐*)s*t.>?:/'☐☐vs*)'s*)?☐/:'☐:/:-;(*)?*<t>./?, F(G{G})A, F☐(G)(G)X/_A☐:-//;☐☐;"-_/ :X:'(BX)::::F(G)XO'T'--BT'BTX/P--"-'F|T|X'F[T][T]X::BT::BT[T]/T'StartedANewOne::
 /8*/8*/8*
 
-'BT[T][TX]'☐☐:☐☐;'PP(red)''BT[TX][TX]'BT[TTX][T'X]'"-(?)/0?/0':-;"-/''☐☐:☐☐;'/PP@2?/_"-_/'(?)0?/'B[TX/M]/?M]'PP'B[/H]T
+'BT[T][TX]'☐☐:☐☐;'PP(red)''BT[TX][TX]'BT[TTX][T'X]'"-(?)/0?/0':-;"-/''☐☐:☐☐;'/PP@2?/_"-_/'(?)0?/'B[TX/M]/?M]'PP'B[/H]T'B[//HHA]TA/?AH
+𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦𓅦'(21ostrich)☐:\-;?/'
 🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒🚒
 🚒🚒🚒🚛🚛🚒🚒🚒🚛🚛🚒🚒🚒🚛🚛🚒🚒🚒🚛🚛
 (PQ'{T}🚒🚒🚒🚛🚛🚒🚒🚒🚛🚛🚒🚒🚒🚛🚛🚒🚒🚒🚛🚛'/P);-l/0)?/':-0;P"-_?/'P:'
@@ -1317,4 +1363,5 @@ FTA??☐☐*)s*t.>?:/'☐☐vs*)'s*)?☐/:'☐:/:-;(*)?*<t>./?, F(G{G})A, F☐(G
 💩💎💩💎💩💎💩💎💩💎💩💎
 🐊💵🐊💵🐊💵🐊💵🐊💵🐊💵
 💵♢💵♢💵♢💵♢💵♢💵♢💵♢
-💵♢🪶💵♢🪶💵♢🪶💵♢🪶\'}}/8*/8*
+💵♢🪶💵♢🪶💵♢🪶💵♢🪶\'}}/8*/8*/8*
+Purpe does;
