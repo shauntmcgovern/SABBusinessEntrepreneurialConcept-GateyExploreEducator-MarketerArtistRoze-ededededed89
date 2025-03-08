75f88273cb12c7429d0c7d418b8830d8b144d9df
X(T)🚒🚒🚒🚒🚒🚒AAAAAAAAAAAATTTTTTTTTTTTTTTTTTTTTTTTTTTT🚒🚒🚒TTTTTTTTTTTTTTTTTTTTTTTTTTTT'TT'|TTTTITX'{TT:}[''':;"'{'}['\:"|"'{'}[':]:'[':]:IMX'{:}['''X:;"'{'}['\:"|"'{'}[':]:I'{' '}['''X:;"'{'}['\:"|"'{'}[':]:'[':]:IꞮT'{;}['''X:;"'{'}['\:"|"'{}🚒🚒🚒🚒|🦍X <shauntmcgovern@gmail.com>
2025-03-08 14:47:53 -0600

    AA'
    AA

---
+++ [Foca(s)'TheFocaVault]/BusinessFileUpdatesandC(#'sharp).js
@@ -0,0 +1,29 @@
+[@#MSAT'
+
+The language of choice is of the "C family" meaning that this language came from the root. From C to C++ and now C#(which has only been around since early 2000')what that means not only does it work fantastic with @Microsoft and even @Apple'iOS actually to program with that side
+
+it is based off of Objective-C which is in the same family yes as the root of "C" the granddaddy. From every language is based off of every other language because it follows the same design pattern the same data discrete structures that uses math and algorithms.
+
+Moving off the topic of ""Swift"". In terms polymorphism can mean certain things inside a C#'codebase really what Roze means to say is which designation is specific and which of these programming terms can mean generic even universal terms covering more ground sotospeak of terms/
+
+Now that gets us on a topic of ""Generics/vt'""'/'""what I am being told is this. Generics serve as a way to compactualize a way of terms that are used again and again over different classes and codebases making it easier for the programmer to understand
+
+during the programming event and events that take place. It also can hold information, ""data'l/v"", that other programming terms, double variables, included which simply can't. It works as a powerhouse and what we are getting into with C#'polymorphism is that we need to house
+
+difference. The difference stemming from what we are looking at and what we are trying to understand as a 'PC' and as it compiles successfully and as we are running a ""train"?/" on it and that this will continue successfully and will continue to execute.
+https://en.wikipedia.org/wiki/C_Sharp_(programming_language)
+
+https://en.wikipedia.org/wiki/C%2B%2B
+
+What is a client doing with a business file update. It depends on what the worker is using usually it falls into two categories, @Microsoft365 which is a collection of the most popularly used office'candidates. From @msexcel to @Microsoft'Word. Then you have to get into
+
+@Apple'--@Microsoft style word documents and Excel spreadsheets. Now that is a different codebase then if your are using a 'Personal PC Computer' at your work office then a laptop supplied by the same @Apple. Although every single document is completely the same and can be easily
+
+transferred from OS' to @Apple'iOS. During an update or an edit or a newSave when a worker from a company is completing their work. The information typed-in, is added to the current document, then the file is a certain type and in that certain coding language. Now we are talking
+
+two different languages one that we have already covered and another that is new. The same difference. Once the save is completed or the edit is done with the file, the selected program, then completes the Action of adding the newly typed business info to the file translating
+
+it into a newly requested that you may use at your behest. Further knowing what happens next is on the coding paradigm and what happens is the file changes the update is then stored inside of the used file because the software just completed that action.//
+https://en.wikipedia.org/wiki/Visual_Basic_for_Applications
+
+'MSAT#@]                
