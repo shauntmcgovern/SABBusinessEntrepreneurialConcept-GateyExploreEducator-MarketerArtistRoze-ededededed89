a1c243f001e04da2407a2aa4ce7ff84a9776dcdb
Shaun {McGovern}'T <shauntmcgovern@gmail.com>
2025-08-02 14:47:52 -0500

    PPA'
    PPA'

--- [Foca(s)'TheFocaVault]/Focas8222025'2025.js
+++ [Foca(s)'TheFocaVault]/Focas8222025'2025.js
@@ -6,18 +6,82 @@ latest(0:)
 2./2. https://usa.visa.com/
 2.https://en.wikipedia.org/wiki/Walmart
 2.https://en.wikipedia.org/wiki/Walmart/'https://www.walmart.com/--
+
+Couldn't of had a better had
+When its just party time
+
+And the remaining of the day
+
+That could be some
+
+Couldn't be a better time
+I said
+
+That wouldn't
+of have been
+that better
+of a tyme
+
+That could be cumbersome
+and the need for it
+
+That couldn't be more
+but what
+more
+are you askin'
+
+I said yes
+
+And the need for it
+was this
+that time
+for that
+more
+
+Couldn't you say
+That need for it
+was the greatest
+
+The greatest of all that was
+
+The need for it
+and the neeed for, it
+
+I said
+
+Time to get together
+and watch the weather
+
+Weather
+
+Weather-us-beings
+
+And then the need for it
+So it comes full circle
+
+Full circle, I said
 :{XX}ATF]:]/'
 
 [::[:/'BT{AX}:-;:-/;"-'L:"/?'PP{PP-0-T"/)"-'_:-;L?/':\|"?'T{X}XX:-;
 P'Contraptions and Bill BoggiensworrthLT{T}D:-;:-0');/;:-"-0-")
 Tiger {LTD}'P
-{:/'💔💔💔
-So what is in the box. The box that had. And it. Serves that significant purpose. Designated. It often doesn't do something other that one or two things but once again you have this for a 'significant' purrpose and the need for it was there. So there. That it
-💔💔💔'\:}
+1. https://www.mcdonalds.com/us/en-us.html\
+2.(2.) https://www.instagram.com/roze.glacierr88leeaaaaa/
+{:/'💔💔💔So what is in the box. The box that had. And it. Serves that significant purpose. Designated. It often doesn't do something other that one or two things but once again you have this for a 'significant' purrpose and the need for it was there. So there. That it💔💔💔'\:}
+{:/'🧡🧡🧡couldn't be better then the last time. The last time. That it was. Now who here, herre likes somethin' that gets not only better with tyme but does this, with any wear and tear annde' like choes it gets not only better over time but it forms to your hand like🧡🧡🧡'\:}
+{:/'🖤🖤🖤a glove. For one instance. You begin to look at 'Kars. But let us take it this way. A wallet how about that how it serves its purpose as a contraption not only that but it does what it does and gets better with'it overtime:09/st' the need for it is there.🖤🖤🖤'\:}
+{:/'💜💜💜Lets (1)name another one. Howabout. Another wearable item that you desperately need comes to mynde'. :09:09/st'(?)Themblindopeners. With a simple'tw{x}ist:09 and completion of the circle to either op[en] facedown or faceup. Now now those are the simplelists'💜💜💜'\:}
+{:/'💙💙💙Thatinordertoaccessthat. As is it isn't all jargon. But in order to spliceit:/(:09)0/?':-;::-;(L0:')TT>/?{TT}W:-?/;Inorder to get the necessary, pieces,, that you do in fact need. For it isn't just a piece of information every bit, spliced, could be very much💙💙💙'\:}   
+{:/'💚💚💚sumthin'. That didn't have any purpose before. So in a sense, that computer operation, could'could be a contraption. A contraption in this sense. And the need for it was there because you take for example the common document although it could lead to proofs💚💚💚'\:}
+{:/'💛💛💛it could also carry significant'' splices that need to be examined because it didn't did not take on this designation before, before it was just a 'Table of Contents' but now your able to see whatitis' if it where broken downinto' yes, Chapters. And in those💛💛💛'\:}
+{:/'💜💜💜chapters significant'' proofs and the need for that and those 'delicate' pieces::-)'; of information then can be used with 'purpose' elsewhere. Because remember before just what did we have yes, something 'General':09sP<T>Car wipers. Schoolhouse janitorial💜💜💜'\:}
+{:/'💗💗💗concerns. These are and this is what it takes to be apart of BiLLBoggienswortthLT{T}D:-/;And the meaning of this as I am copying this 'verbatim:09 is that this is what it has come down to. Shaun can do this job alot more fluently if he had his essential💗💗💗'\:}
+{:/'🖤🖤🖤materials. But he doesn/t have that now doesn't he. Nor does Shaun have any ability to apply for a {J}ob and he just recently setup an interview at MicceD's::@McDonalds where he just saw his Margot Robbie' and the interview did not go thru and it didn't go🖤🖤🖤'\:}
+{:/'💗💗💗to well with 'Moses':09./st' Either. So moving on. He is rather a young Boss. Nice and young. So its a little more of a 'Skip and Jump,' then rather a boss that is terminally older than you are and the call remaining from the term and fact is this. How is that💗💗💗'\:}
+{:/'🧡🧡🧡and what couldn't be more. And what do you exactly do' I mean what is it, exactly. Why you run the messageServices' without any monetary compensation its actually going to the Homeless' and it has to go somewhere Shaun, the money must go somewhere. Annde' 🧡🧡🧡'\:}
+{:/'🧡🧡🧡{GATTEY}:09 is worRK-i-ng for FRee. A freeworker. So you know how frustrating this is and he has to live on bags. Bags of supplies. Life Supplies. And the need fo0r that is there. So this is what you do and this is what is there, for you. The need for this🧡🧡🧡'\:}
+Then a nice Pane of McDonalds. 
 
-{:/'🧡🧡🧡couldn't be better then the last time. The last time. 
-
-🧡🧡🧡'\:}
 
 {:/'💗💗💗
 
@@ -57,5 +121,6 @@ So what is in the box. The box that had. And it. Serves that significant purpose
 ;-:XX}X{T'?"|\:'/?L;-:_'-")/"T-0-PP{PP'?/":L'-";/-:;-:}XA{TB'/:]::]'PP{X}(PPX):09/0'
 
 🧡❤💛💚💙💜💔🖤❤️
+
 
 
