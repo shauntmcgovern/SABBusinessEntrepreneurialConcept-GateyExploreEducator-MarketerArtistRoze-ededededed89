365af5184b0ae1eb103f1d68f3e0929f40053e6d
Shaun {McGovernn:}:'}'TP'A{:A'/}:-;'{XX|:X{:X;}+/='::P('PPA:X:';?/')"-'"-'P <shauntmcgovern@gmail.com>
2026-02-14 14:31:22 -0600

    'AA
    'AA(AST)How to run the TokenFetchscript in the CSS or HTML or JS'File and how to setup an example react-chartjs-2 "Chart" that you can then change in 'realtime

--- SkillsTest/App.js
+++ SkillsTest/App.js
@@ -1,5 +1,16 @@
+//the function of the React'App
 import logo from './logo.svg';
 import './App.css';
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";//I do see this library installed in my project
+import { Doughnut } from "react-chartjs-2";//I do not see this library installed in my project-- so I did this now it is there and avail'npm i react-chartjs-2 chart.js)):/')):-;
+
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+//what this does is run the ChartJS-'method' the register method that requires the ArcElement and Tooltip and Legend to pass on through this Javascript'React'Method
+
+
+//<Doughnut data={...} />//this isn't ready yet I am assuming here, you enter the 'information' with the '...' to complete this
 
 function App() {
   return (
